df5614f828fc9736c80f2ee5545a8c8383545845
Carl Howard <choward8@ycp.edu>
2014-03-27 12:41:50 -0400

    Making the world go boom and made a sign up page

--- TGOHProject/src/edu/ycp/cs/cs496/TGOH/MainActivity.java
+++ TGOHProject/src/edu/ycp/cs/cs496/TGOH/MainActivity.java
@@ -3,6 +3,8 @@ package edu.ycp.cs.cs496.TGOH;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,23 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void setDefaultView(){
+		setContentView(R.layout.activity_main);
+		
+		Button Signin = (Button) findViewById(R.id.button1);
+		Button Signup = (Button) findViewById(R.id.button2);
+		
+		Signup.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.signuppage);
+				// setting a new account to the Database.
+				
+			}
+		});
 	}
 
 }
