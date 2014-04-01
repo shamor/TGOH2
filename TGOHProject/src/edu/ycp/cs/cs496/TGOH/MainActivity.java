f69fc365f6afd30c9810e6d7f85fa03011124359
Samantha Hamor <shamor1@ycp.edu>
2014-04-01 13:01:59 -0400

    test

--- TGOHProject/src/edu/ycp/cs/cs496/TGOH/MainActivity.java
+++ TGOHProject/src/edu/ycp/cs/cs496/TGOH/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setDefaultView();
 	}
-
+	//hello test
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
