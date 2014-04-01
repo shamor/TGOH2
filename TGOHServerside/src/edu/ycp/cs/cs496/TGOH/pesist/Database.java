642ca9ac250a3ff1133805c4f5b8568fa5a3cd42
Carl Howard <choward8@ycp.edu>
2014-04-01 12:22:25 -0400

    made the sever side

---
+++ TGOHServerside/src/edu/ycp/cs/cs496/TGOH/pesist/Database.java
@@ -0,0 +1,17 @@
+package edu.ycp.cs.cs496.TGOH.pesist;
+
+/**
+ * Allow access to the singleton {@link IDatabase} implementation.
+ */
+public class Database {
+	private static final IDatabase theInstance = new FakeDatabase();
+	
+	/**
+	 * Get the singleton {@link IDatabase} implementation.
+	 * 
+	 * @return the singleton {@link IDatabase} implementation
+	 */
+	public static IDatabase getInstance() {
+		return theInstance;
+	}
+}
