ba1d8b5b634a0446d1036760781fdc01010d6e34
Samantha Hamor <shamor1@ycp.edu>
2014-04-24 13:19:38 -0400

    Fixed a few errors post merge

--- TGOHServerside/src/edu/ycp/cs/cs496/TGOH/controller/getCourse.java
+++ TGOHServerside/src/edu/ycp/cs/cs496/TGOH/controller/getCourse.java
@@ -2,10 +2,11 @@ package edu.ycp.cs.cs496.TGOH.controller;
 
 import edu.ycp.cs.cs496.TGOH.pesist.Database;
 import edu.ycp.cs.cs496.TGOH.pesist.IDatabase;
+import edu.ycp.cs.cs496.TGOH.temp.Courses;
 
 public class getCourse {
 	public String getCourseName(String Username, String Course){
 		IDatabase db = Database.getInstance();
-		return db.getCourse(Username, Course);
+		return db.getCourse(Username, Course).getCourse(0);
 	}
 }
