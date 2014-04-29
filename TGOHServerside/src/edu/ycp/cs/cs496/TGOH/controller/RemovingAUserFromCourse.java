2f673bca1c81599845c8828021a72285b53616d2
Carl Howard <choward8@ycp.edu>
2014-04-29 13:13:31 -0400

    fixing up the database. making the selvets simplified

--- TGOHServerside/src/edu/ycp/cs/cs496/TGOH/controller/RemovingAUserFromCourse.java
+++ TGOHServerside/src/edu/ycp/cs/cs496/TGOH/controller/RemovingAUserFromCourse.java
@@ -3,10 +3,11 @@ package edu.ycp.cs.cs496.TGOH.controller;
 import edu.ycp.cs.cs496.TGOH.pesist.Database;
 import edu.ycp.cs.cs496.TGOH.pesist.IDatabase;
 import edu.ycp.cs.cs496.TGOH.temp.Courses;
+import edu.ycp.cs.cs496.TGOH.temp.User;
 
-public class getCourse {
-	public String getCourseName(String Username, String Course){
+public class RemovingAUserFromCourse {
+	public void RemovingUserFromCourse(User user, Courses course){
 		IDatabase db = Database.getInstance();
-		return db.getCourse(Username, Course);
+		db.RemovingUserFromCourse(user, course);
 	}
 }
